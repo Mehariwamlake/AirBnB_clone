30000dea6fb164903dd34330bc4eb2c5539da29f
mehariw <mehariwmalke@gmail.com>
2022-08-04 01:33:01 -0400

    Best school

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,41 @@
 #!/usr/bin/python3
-""" Module for Base """
-import uuid
-import datetime
-import models
+"""
+Contains class BaseModel
+"""
 
-format_dt = "%Y-%m-%dT%H:%M:%S.%f"
+from datetime import datetime
+import models
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
-    """ Basemodel class """
+    """The BaseModel class from which future classes will be derived"""
+
     def __init__(self, *args, **kwargs):
-        """ Initialization of Database """
-        if args is not None and len(args) > 0:
-            pass
+        """Initialization of the base model"""
         if kwargs:
-            for key, item in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    item = datetime.strptime(item, format_dt)
-                if key not in ['__class__']:
-                    setattr(self, key, item)
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
+            models.storage.save()
+
+    def __str__(self):
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
+
+    def save(self):
+        """updates the attribute 'updated_at' with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
